5aa9532d6646b238f9d5611777af38e03f77062f
Nick Barnes <Nicholas.Barnes4194@.com>
2021-06-27 17:03:53 -0400

    kinda works but meets mvp

--- src/components/Location.tsx
+++ src/components/Location.tsx
@@ -1,14 +1,16 @@
 import React, { Component  } from 'react'
 
 type Weather = {
-    locationWeather:{
-        weather:{
-            description: string;
-        }
-        main:{
-            temp:string
-        }
-    }
+    // locationWeather:{
+    //     weather:{
+    //         description: string;
+    //     }
+    //     main:{
+    //         temp:string
+    //     }
+    // }
+    temp: number,
+    description: string,
     currentLat: number,
     currentLong: number
 }
@@ -26,14 +28,8 @@ export default class Location extends Component<{}, Weather>{
         constructor(props: {} ){
             super(props);
             this.state={
-                locationWeather:{
-                    weather:{
-                        description: ""
-                    },
-                    main:{
-                        temp:""
-                    }
-                },
+                temp: 0,
+                description: "",
                 currentLat: 0,
                 currentLong: 0 
             }
@@ -49,11 +45,13 @@ export default class Location extends Component<{}, Weather>{
                 
             }
 
-            fetchWeather(){
+            fetchWeather = async () => {
                 let url = `https://api.openweathermap.org/data/2.5/weather?lat=${this.state.currentLat}&lon=${this.state.currentLong}&appid=${}` //adding api key later so that i can push to github
-                
-                fetch(url)
+
+                await fetch(url)
                 .then((res) => res.json())
+                .then( (res) => this.setState({temp: res.main.temp})) //description: res.weather.description}))
+                
                 // .then((fetchData) => data: string)
 
             }
@@ -70,12 +68,14 @@ export default class Location extends Component<{}, Weather>{
         // }
         componentDidMount(){
             navigator.geolocation.getCurrentPosition(this.success);
-            // this.fetchWeather()
+            this.fetchWeather()
         }
     render(){
         return(
             <div>
                 <h1>I am this location {this.state.currentLat}, {this.state.currentLong} </h1>
+                <h1>This is the temperature: {this.state.temp} degrees </h1>
+                {/* <h1>This is what it looks like : {this.state.description}</h1> */}
             </div>
         )
     }
